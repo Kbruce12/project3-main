b21ebe705915bbd1481c883f6765eaf9c9f1f6c3
KevinBruce <kevinlawbruce@gmail.com>
2022-08-09 14:35:16 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
+
 function App() {
+  const newUrl = 'https://api.lyrics.ovh/v1/biffy clyro/folding stars';
+  axios({
+    url: newUrl,
+    method: 'GET',
+    dataType: 'JSON'
+  }).then((response) => {
+    console.log(response.data.lyrics);
+  })
+
+
+
+
+
+
+
+
+
+  // const lyricsGenerator =  () => {
+  //   const url = new URL (`https://api.lyrics.ovh/v1/foo fighters/walk`);
+
+  //   fetch(url)
+  //   .then((response) => {
+  //     return (response.json);
+  //   })
+  // }
+  // lyricsGenerator();
+  
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
